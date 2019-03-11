40f2adc1773fd37fb8a01674231b32d02ec63a61
Gittoks <ilja.batalev@gmail.com>
2019-03-11 12:27:01 +0300

    add basic structs

---
+++ src/structs.h
@@ -0,0 +1,19 @@
+typedef struct {
+	int lenght;
+	int* numbers;
+} NaturalNumber;
+
+typedef struct {
+	int sign;
+	NaturalNumber naturalNumber;
+} WholeNumber;
+
+typedef struct {
+	NaturalNumber naturalNumber;
+	WholeNumber WholeNumber;
+} RationNumber;
+
+typedef struct {
+	int lenght;
+	RationNumber* rationNumbers;
+} Multinomial;
